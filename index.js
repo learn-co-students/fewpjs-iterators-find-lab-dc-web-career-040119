2545d7e80eb4b0363dcffc7d98ac1417230b237c
github username <github email address>
2019-05-12 18:17:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+//Objective: find a year where the Chiefs won a SuperBowl and set it to a var superbowlWin
+
+// take the array of objects in record and set it to a var result
+// .find the array where the record.result is a W (for 'win')
 
 let superbowlWin = record => {
   let result = record.find( record => record.result === "W" )
+  //using a ternary operator, ask if there is a result? If so, return the result.year
+  // otherwise return 'undefined'
   return !!result ? result.year : undefined
 }
