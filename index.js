a9b0b9f1b48da7f17ab96f78336f33e7bbf9e985
Marior9 <mariorodriguezan@gmail.com>
2019-05-13 17:09:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+console.log('connected')
+
 const testVar = {}
 
 function testFunc() {
   return "hi"
+}
+
+function superbowlWin(record){
+  let win = record.find(record => record.result === "W")
+    if(win){
+    return win.year
+    }else{
+    return undefined
+    }
+
 }
