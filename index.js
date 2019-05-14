192cabfc7c04690af2eedce146215e2a21e1c1ad
Matthew Lindsey <mbl@null.net>
2019-05-14 15:22:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const testVar = {}
+const record = [
+  { year: '2018', result: 'L' },
+  { year: '2017', result: 'W' },
+  { year: '2016', result: 'N/A' },
+];
 
-function testFunc() {
-  return "hi"
-}
+function superbowlWin(record) {
+  const match = record.find(record => record.result === 'W');
+  return !!match ? match.year : undefined;
+};
+
+console.log(superbowlWin(record));
